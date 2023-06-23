f88cda9e559cf8c44802176dccb6ac9d8ebaa18e
lmngxn <lmngxn@gmail.com>
2023-06-23 08:31:09 +0800

    Buy Sell (Flask) Template
    Using flask to build a buy/sell and transaction records for stocks

---
+++ finance/app.py
@@ -0,0 +1,237 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import apology, login_required, lookup, usd
+
+# Configure application
+app = Flask(__name__)
+
+# Custom filter
+app.jinja_env.filters["usd"] = usd
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///finance.db")
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/")
+@login_required
+def index():
+    """Show portfolio of stocks"""
+    stocks = db.execute("SELECT name, symbol, shares FROM stocks WHERE user_id=?", session["user_id"])
+    cash = db.execute("SELECT cash FROM users WHERE id=?", session["user_id"])[0]['cash']
+    total = cash
+    for stock in stocks:
+        temp = lookup(stock['symbol'])
+        price = temp['price']
+        value = price * stock['shares']
+        stock['price'] = price
+        stock['value'] = value
+        total += value
+    return render_template("stocks.html", stocks=stocks, cash=cash, total=total)
+
+
+@app.route("/buy", methods=["GET", "POST"])
+@login_required
+def buy():
+    """Buy shares of stock"""
+    if request.method == "POST":
+
+        # Ensure something was entered into symbol field
+        if not request.form.get("symbol"):
+            return apology("please provide stock's symbol", 403)
+
+        if not request.form.get("shares") or int(request.form.get("shares")) < 1:
+            return apology("please provide positive number of shares", 403)
+
+        # Query Yahoo API for stock info
+        stock = lookup(request.form.get("symbol"))
+        shares = int(request.form.get("shares"))
+        cash = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])[0]['cash']
+
+        if not stock:
+            return apology("please provide stock's symbol", 403)
+        elif stock['price'] * shares > cash:
+            return apology("you have insufficient funds", 403)
+        else:
+            db.execute("INSERT INTO transactions VALUES (?, ?, ?, ?, ?, ?)", session["user_id"], "BUY", stock['name'], stock['symbol'], shares, stock['price'])
+            cash = cash - (stock['price'] * shares)
+            db.execute("UPDATE users SET cash = ? WHERE id = ?", cash, session["user_id"])
+            exist = db.execute("SELECT * FROM stocks WHERE user_id = ? AND symbol = ?", session["user_id"], stock['symbol'])
+            if len(exist) == 0:
+                db.execute("INSERT INTO stocks (user_id, name, symbol, shares) VALUES (?, ?, ?, ?)", session["user_id"], stock['name'], stock['symbol'], shares)
+            else:
+                db.execute("UPDATE stocks SET shares = ? WHERE user_id = ? AND symbol = ?", shares + exist[0]['shares'], session["user_id"], stock['symbol'])
+            return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("buy.html")
+
+@app.route("/history")
+@login_required
+def history():
+    """Show history of transactions"""
+    transactions = db.execute("SELECT * FROM transactions WHERE user_id=?", session["user_id"])
+    return render_template("history.html", transactions=transactions)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+@app.route("/quote", methods=["GET", "POST"])
+@login_required
+def quote():
+    """Get stock quote."""
+    if request.method == "POST":
+
+        # Ensure something was entered into symbol field
+        if not request.form.get("symbol"):
+            return render_template("quote.html")
+
+        # Query Yahoo API for stock info
+        stock = lookup(request.form.get("symbol"))
+
+        if not stock:
+            return render_template("quote.html")
+        else:
+            return render_template("quoted.html", stock=stock)
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("quote.html")
+
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        elif request.form.get("password") != request.form.get("confirmation"):
+            return apology("password do not match", 403)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) > 0:
+            return apology("username already exists", 403)
+
+        # Remember which user has logged in
+        db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", request.form.get("username"), generate_password_hash(request.form.get("password")))
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
+
+
+@app.route("/sell", methods=["GET", "POST"])
+@login_required
+def sell():
+    """Sell shares of stock"""
+    if request.method == "POST":
+
+        # Ensure something was entered into symbol field
+        if not request.form.get("symbol"):
+            return apology("please provide stock's symbol", 403)
+
+        if not request.form.get("shares") or int(request.form.get("shares")) < 1:
+            return apology("please provide positive number of shares", 403)
+
+        # Query Yahoo API for stock info
+        stock = lookup(request.form.get("symbol"))
+        to_sell = int(request.form.get("shares"))
+        shares = db.execute("SELECT shares FROM stocks WHERE user_id = ? AND symbol = ?", session["user_id"], stock['symbol'])[0]['shares']
+
+        if not stock:
+            return apology("please provide stock's symbol", 403)
+        elif shares == 0:
+            return apology("you do not have enough shares", 403)
+        else:
+            db.execute("INSERT INTO transactions VALUES (?, ?, ?, ?, ?, ?)", session["user_id"], "SELL", stock['name'], stock['symbol'], to_sell, stock['price'])
+            cash = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])[0]['cash']
+            cash += stock['price'] * to_sell
+            db.execute("UPDATE users SET cash = ? WHERE id = ?", cash, session["user_id"])
+            if shares - to_sell == 0:
+                db.execute("DELETE FROM stocks WHERE user_id = ? AND symbol = ?", session["user_id"], stock)
+            else:
+                db.execute("UPDATE stocks SET shares = ? WHERE user_id = ? AND symbol = ?", shares - to_sell, session["user_id"], stock['symbol'])
+            return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("sell.html")
